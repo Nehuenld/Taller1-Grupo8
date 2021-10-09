1dd7d562bb5524b81fd91ba7dbc73cca0ebb93c9
facubeltran <facundocristianbeltran@gmail.com>
2021-10-09 04:43:03 -0300

    cambio2

---
+++ TPcontacto/jsContacto.js
@@ -0,0 +1,5 @@
+function mostrarDatos(){
+    let nombre = document.getElementById("nombre");
+    let email=  document.getElementById("email"); // no me agarra me parece comprobar
+    let comentario = document.getElementById("comentario");
+}
---
+++ javascript/jsPaginaProducto.js
@@ -0,0 +1,7 @@
+// Test de usar javascript para tomar el elementoProducto(titulo) y 
+// posicionarlo en la paginaProducto(pagina Joha), pero todavia no encontre solucion
+
+
+/*let nombreProducto = document.getElementById(Candyman);
+console.log(nombreProducto);
+*/
