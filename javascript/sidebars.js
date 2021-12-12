49819f4c50bb70ee3a34d51b99b766dd252e74ca
Neus <onamonzon.32@gmail.com>
2021-12-12 20:18:36 -0300

    mostrar el json en la pagina de admins

--- javascript/sidebars.js
+++ javascript/sidebars.js
@@ -5,53 +5,57 @@
     tooltipTriggerList.forEach(function(tooltipTriggerEl) {
         new bootstrap.Tooltip(tooltipTriggerEl)
     })
+    
 })()
 
+function mostrarJson(){
+    const sectionGrid = document.getElementById('verJson');
+    const requestURL = 'https://api.themoviedb.org/4/list/7113794?language=es&api_key=dc3b22860cde29d18263951f0f48ee4a&page=1';
+    const request = new XMLHttpRequest(); //instancian objeto 
+    let jsonStr = '';
+    request.open('GET', requestURL); // setean el método, la url de api
+    request.responseType = 'json'; //definen el tipo de dato que les devuelve
+    request.send(); //envían la solicitud
+    request.onload = function() { //esperan la respuesta
+        jsonStr = request.response;
+        let jsonView = JSON.stringify(jsonStr);
+        console.log(jsonView);       
+        
+        //sectionGrid.innerHTML = jsonView;
+            
 
-const sectionGrid = document.getElementById('verJson');
-const requestURL = 'https://api.themoviedb.org/4/list/7113794?language=es&api_key=dc3b22860cde29d18263951f0f48ee4a&page=1';
-const request = new XMLHttpRequest(); //instancian objeto 
-var jsonStr = '';
-request.open('GET', requestURL); // setean el método, la url de api
-request.responseType = 'json'; //definen el tipo de dato que les devuelve
-request.send(); //envían la solicitud
-request.onload = function() { //esperan la respuesta
-    jsonStr = JSON.stringify( request.response);
-           
-    console.log(jsonStr);
-          
+
+    regeStr = '', // A EMPTY STRING TO EVENTUALLY HOLD THE FORMATTED STRINGIFIED OBJECT
+        f = {
+                brace: 0
+            }; // AN OBJECT FOR TRACKING INCREMENTS/DECREMENTS,
+            // IN PARTICULAR CURLY BRACES (OTHER PROPERTIES COULD BE ADDED)
+
+    regeStr = jsonView.replace(/({|}[,]*|[^{}:]+:[^{}:,]*[,{]*)/g, function (m, p1) {
+    var rtnFn = function() {
+            return '<div style="text-indent: ' + (f['brace'] * 20) + 'px;">' + p1 + '</div>';
+        },
+        rtnStr = 0;
+        if (p1.lastIndexOf('{') === (p1.length - 1)) {
+            rtnStr = rtnFn();
+            f['brace'] += 1;
+        } else if (p1.indexOf('}') === 0) {
+            f['brace'] -= 1;
+            rtnStr = rtnFn();
+        } else {
+            rtnStr = rtnFn();
+        }
+        return rtnStr;
+    });
+
+    
+
+        sectionGrid.innerHTML += regeStr; // inserta el resultado en el HTML
+    }
+    
 }
+   
 
-       
-sectionGrid.innerHTML = jsonStr;
-
-        /*
-        let contCard = document.createElement('div');
-        contCard.className = 'row';
-       
-        function mostrarMovies() {
-            let pelicula = movies.results;
-            for (i = 0; i < pelicula.length && i < 6; i++) {
-                let cardDiv = document.createElement('div');
-               cardDiv.className = "col-lg-4 col-md-8";
-                let image = 'https://image.tmdb.org/t/p/w500/' + pelicula[i].poster_path;
-                let title = pelicula[i].original_title;
-                
-
-                cardDiv.innerHTML = `<a href="PaginaProductoIndividual.html?id=${i}" target="_blank">
-                                        <img class="imagen" src="${image}" alt="">
-                                        <div class="img-overlay img-overlay-blur">
-                                            <div  class="img-title">${title}</div>
-                                        <p class="img-subtitle">Reservar</p>
-                                        </div>
-                                    </a>`;
-              
-                contCard.appendChild(cardDiv);
-                //sectionGrid.appendChild(contCard);
-
-            }
-
-        }*/
 
 //obtener parametros (login.js)
 let idUserDetected = getParameterByName('id');
