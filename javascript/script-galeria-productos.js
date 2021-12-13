63691f7134d30b993e0b102c7ec8748fc9836249
Nehuenld <deposito3@ALFAPINTURERIA.LOCAL>
2021-12-13 13:25:28 -0300

    orden ascendente descendente Nehuen desde Depo3

--- javascript/script-galeria-productos.js
+++ javascript/script-galeria-productos.js
@@ -14,14 +14,14 @@ request.onload = function() {
     //listadoPelis = movies.results;
     console.log(movies);
     //console.log(listadoPelis);
-    crearImagenes();
+  crearImagenes();
+   
 }
 
 
 
 function crearImagenes() {
     let listadoPeliculas = movies.results;
-
     for (let i = 0; i < listadoPeliculas.length; i++) {
         let myDivRow1 = document.createElement("div");
         myDivRow1.className = "col-lg-3 col-md-6 col-xs-2";
@@ -53,3 +53,48 @@ function crearImagenes() {
 
     }
 }
+    
+    function ordenAsc() {
+        row.innerHTML = "";
+        let peliculas = movies.results;
+        let listadoPeliculas = peliculas.sort((a, b) => a - b);
+        console.log(listadoPeliculas);
+        for (let i = 0; i < listadoPeliculas.length; i++) {
+            let myDivRow1 = document.createElement("div");
+            myDivRow1.className = "col-lg-3 col-md-6 col-xs-2";
+    
+            let myLink = document.createElement("a");
+            myLink.href = "PaginaProductoIndividual.html?id=" + i;
+            myLink.target = "_blank";
+    
+            let myImg = document.createElement("img");
+            myImg.src = 'https://image.tmdb.org/t/p/w500/' + listadoPeliculas[i].poster_path;
+    
+            let myDivOverlay = document.createElement("div");
+            myDivOverlay.className = "img-overlay img-overlay-blur";
+    
+            let myDivImgTitle = document.createElement("div");
+            myDivImgTitle.className = "img-title";
+            myDivImgTitle.textContent = listadoPeliculas[i].title;
+    
+            let myDivImgSubtitle = document.createElement("div");
+            myDivImgSubtitle.className = "img-subtitle";
+            myDivImgSubtitle.textContent = "Reservar";
+    
+            row.appendChild(myDivRow1);
+            myDivRow1.appendChild(myLink);
+            myLink.appendChild(myImg);
+            myLink.appendChild(myDivOverlay);
+            myDivOverlay.appendChild(myDivImgTitle);
+            myDivOverlay.appendChild(myDivImgSubtitle);
+    
+        }
+    }
+    
+    function ordenDsc() {
+        row.innerHTML = "";
+        ordenAsc().reverse;
+    }
+    
+
+    
