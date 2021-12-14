d1298e9340686b7bcb675a41b61581280dbac7f4
Rocío Catalano <rociocatalano4@hotmail.com>
2021-12-14 14:47:31 -0300

    funcionalidad del resto de los botones

--- javascript/script-galeria-productos.js
+++ javascript/script-galeria-productos.js
@@ -375,3 +375,292 @@ function pelisSuspenso() {
         myDivOverlay.appendChild(myDivImgSubtitle);
     }
 }
+
+function idiomaIng() {
+    row.innerHTML = "";
+    let peliculas = movies.results;
+    let listadoPeliculas = [];
+
+    for (let i = 0; i < peliculas.length; i++) {
+        if (peliculas[i].original_language == "en") {
+            listadoPeliculas.push(peliculas[i]);
+        }
+    }
+    for (let i = 0; i < listadoPeliculas.length; i++) {
+        let myDivRow1 = document.createElement("div");
+        myDivRow1.className = "col-lg-3 col-md-6 col-xs-2";
+
+        let myLink = document.createElement("a");
+        myLink.href = "PaginaProductoIndividual.html?id=" + i;
+        myLink.target = "_blank";
+
+        let myImg = document.createElement("img");
+        myImg.src = 'https://image.tmdb.org/t/p/w500/' + listadoPeliculas[i].poster_path;
+
+        let myDivOverlay = document.createElement("div");
+        myDivOverlay.className = "img-overlay img-overlay-blur";
+
+        let myDivImgTitle = document.createElement("div");
+        myDivImgTitle.className = "img-title";
+        myDivImgTitle.textContent = listadoPeliculas[i].title;
+
+        let myDivImgSubtitle = document.createElement("div");
+        myDivImgSubtitle.className = "img-subtitle";
+        myDivImgSubtitle.textContent = "Reservar";
+
+        row.appendChild(myDivRow1);
+        myDivRow1.appendChild(myLink);
+        myLink.appendChild(myImg);
+        myLink.appendChild(myDivOverlay);
+        myDivOverlay.appendChild(myDivImgTitle);
+        myDivOverlay.appendChild(myDivImgSubtitle);
+    }
+
+}
+
+function idiomaEs() {
+    row.innerHTML = "";
+    let peliculas = movies.results;
+    let listadoPeliculas = [];
+
+    for (let i = 0; i < peliculas.length; i++) {
+        if (peliculas[i].original_language == "es") {
+            listadoPeliculas.push(peliculas[i]);
+        }
+    }
+    for (let i = 0; i < listadoPeliculas.length; i++) {
+        let myDivRow1 = document.createElement("div");
+        myDivRow1.className = "col-lg-3 col-md-6 col-xs-2";
+
+        let myLink = document.createElement("a");
+        myLink.href = "PaginaProductoIndividual.html?id=" + i;
+        myLink.target = "_blank";
+
+        let myImg = document.createElement("img");
+        myImg.src = 'https://image.tmdb.org/t/p/w500/' + listadoPeliculas[i].poster_path;
+
+        let myDivOverlay = document.createElement("div");
+        myDivOverlay.className = "img-overlay img-overlay-blur";
+
+        let myDivImgTitle = document.createElement("div");
+        myDivImgTitle.className = "img-title";
+        myDivImgTitle.textContent = listadoPeliculas[i].title;
+
+        let myDivImgSubtitle = document.createElement("div");
+        myDivImgSubtitle.className = "img-subtitle";
+        myDivImgSubtitle.textContent = "Reservar";
+
+        row.appendChild(myDivRow1);
+        myDivRow1.appendChild(myLink);
+        myLink.appendChild(myImg);
+        myLink.appendChild(myDivOverlay);
+        myDivOverlay.appendChild(myDivImgTitle);
+        myDivOverlay.appendChild(myDivImgSubtitle);
+    }
+
+}
+
+function idiomaOtros() {
+    row.innerHTML = "";
+    let peliculas = movies.results;
+    let listadoPeliculas = [];
+
+    for (let i = 0; i < peliculas.length; i++) {
+        if (peliculas[i].original_language != "es" && peliculas[i].original_language != "en") {
+            listadoPeliculas.push(peliculas[i]);
+        }
+    }
+    for (let i = 0; i < listadoPeliculas.length; i++) {
+        let myDivRow1 = document.createElement("div");
+        myDivRow1.className = "col-lg-3 col-md-6 col-xs-2";
+
+        let myLink = document.createElement("a");
+        myLink.href = "PaginaProductoIndividual.html?id=" + i;
+        myLink.target = "_blank";
+
+        let myImg = document.createElement("img");
+        myImg.src = 'https://image.tmdb.org/t/p/w500/' + listadoPeliculas[i].poster_path;
+
+        let myDivOverlay = document.createElement("div");
+        myDivOverlay.className = "img-overlay img-overlay-blur";
+
+        let myDivImgTitle = document.createElement("div");
+        myDivImgTitle.className = "img-title";
+        myDivImgTitle.textContent = listadoPeliculas[i].title;
+
+        let myDivImgSubtitle = document.createElement("div");
+        myDivImgSubtitle.className = "img-subtitle";
+        myDivImgSubtitle.textContent = "Reservar";
+
+        row.appendChild(myDivRow1);
+        myDivRow1.appendChild(myLink);
+        myLink.appendChild(myImg);
+        myLink.appendChild(myDivOverlay);
+        myDivOverlay.appendChild(myDivImgTitle);
+        myDivOverlay.appendChild(myDivImgSubtitle);
+    }
+}
+
+function destacados() {
+    row.innerHTML = "";
+    let peliculas = movies.results;
+    let listadoPeliculas = [];
+
+    for (let i = 0; i < 6; i++) {
+        listadoPeliculas.push(peliculas[i]);
+    }
+
+    for (let i = 0; i < listadoPeliculas.length; i++) {
+        let myDivRow1 = document.createElement("div");
+        myDivRow1.className = "col-lg-3 col-md-6 col-xs-2";
+
+        let myLink = document.createElement("a");
+        myLink.href = "PaginaProductoIndividual.html?id=" + i;
+        myLink.target = "_blank";
+
+        let myImg = document.createElement("img");
+        myImg.src = 'https://image.tmdb.org/t/p/w500/' + listadoPeliculas[i].poster_path;
+
+        let myDivOverlay = document.createElement("div");
+        myDivOverlay.className = "img-overlay img-overlay-blur";
+
+        let myDivImgTitle = document.createElement("div");
+        myDivImgTitle.className = "img-title";
+        myDivImgTitle.textContent = listadoPeliculas[i].title;
+
+        let myDivImgSubtitle = document.createElement("div");
+        myDivImgSubtitle.className = "img-subtitle";
+        myDivImgSubtitle.textContent = "Reservar";
+
+        row.appendChild(myDivRow1);
+        myDivRow1.appendChild(myLink);
+        myLink.appendChild(myImg);
+        myLink.appendChild(myDivOverlay);
+        myDivOverlay.appendChild(myDivImgTitle);
+        myDivOverlay.appendChild(myDivImgSubtitle);
+    }
+}
+
+function punt8() {
+    row.innerHTML = "";
+    let peliculas = movies.results;
+    let listadoPeliculas = [];
+
+    for (let i = 0; i < peliculas.length; i++) {
+        if (peliculas[i].vote_average > 8) {
+            listadoPeliculas.push(peliculas[i]);
+        }
+    }
+    for (let i = 0; i < listadoPeliculas.length; i++) {
+        let myDivRow1 = document.createElement("div");
+        myDivRow1.className = "col-lg-3 col-md-6 col-xs-2";
+
+        let myLink = document.createElement("a");
+        myLink.href = "PaginaProductoIndividual.html?id=" + i;
+        myLink.target = "_blank";
+
+        let myImg = document.createElement("img");
+        myImg.src = 'https://image.tmdb.org/t/p/w500/' + listadoPeliculas[i].poster_path;
+
+        let myDivOverlay = document.createElement("div");
+        myDivOverlay.className = "img-overlay img-overlay-blur";
+
+        let myDivImgTitle = document.createElement("div");
+        myDivImgTitle.className = "img-title";
+        myDivImgTitle.textContent = listadoPeliculas[i].title;
+
+        let myDivImgSubtitle = document.createElement("div");
+        myDivImgSubtitle.className = "img-subtitle";
+        myDivImgSubtitle.textContent = "Reservar";
+
+        row.appendChild(myDivRow1);
+        myDivRow1.appendChild(myLink);
+        myLink.appendChild(myImg);
+        myLink.appendChild(myDivOverlay);
+        myDivOverlay.appendChild(myDivImgTitle);
+        myDivOverlay.appendChild(myDivImgSubtitle);
+    }
+}
+
+function punt6() {
+    row.innerHTML = "";
+    let peliculas = movies.results;
+    let listadoPeliculas = [];
+
+    for (let i = 0; i < peliculas.length; i++) {
+        if (peliculas[i].vote_average > 6) {
+            listadoPeliculas.push(peliculas[i]);
+        }
+    }
+    for (let i = 0; i < listadoPeliculas.length; i++) {
+        let myDivRow1 = document.createElement("div");
+        myDivRow1.className = "col-lg-3 col-md-6 col-xs-2";
+
+        let myLink = document.createElement("a");
+        myLink.href = "PaginaProductoIndividual.html?id=" + i;
+        myLink.target = "_blank";
+
+        let myImg = document.createElement("img");
+        myImg.src = 'https://image.tmdb.org/t/p/w500/' + listadoPeliculas[i].poster_path;
+
+        let myDivOverlay = document.createElement("div");
+        myDivOverlay.className = "img-overlay img-overlay-blur";
+
+        let myDivImgTitle = document.createElement("div");
+        myDivImgTitle.className = "img-title";
+        myDivImgTitle.textContent = listadoPeliculas[i].title;
+
+        let myDivImgSubtitle = document.createElement("div");
+        myDivImgSubtitle.className = "img-subtitle";
+        myDivImgSubtitle.textContent = "Reservar";
+
+        row.appendChild(myDivRow1);
+        myDivRow1.appendChild(myLink);
+        myLink.appendChild(myImg);
+        myLink.appendChild(myDivOverlay);
+        myDivOverlay.appendChild(myDivImgTitle);
+        myDivOverlay.appendChild(myDivImgSubtitle);
+    }
+
+}
+
+function puntOtros() {
+    row.innerHTML = "";
+    let peliculas = movies.results;
+    let listadoPeliculas = [];
+
+    for (let i = 0; i < peliculas.length; i++) {
+        if (peliculas[i].vote_average < 6) {
+            listadoPeliculas.push(peliculas[i]);
+        }
+    }
+    for (let i = 0; i < listadoPeliculas.length; i++) {
+        let myDivRow1 = document.createElement("div");
+        myDivRow1.className = "col-lg-3 col-md-6 col-xs-2";
+
+        let myLink = document.createElement("a");
+        myLink.href = "PaginaProductoIndividual.html?id=" + i;
+        myLink.target = "_blank";
+
+        let myImg = document.createElement("img");
+        myImg.src = 'https://image.tmdb.org/t/p/w500/' + listadoPeliculas[i].poster_path;
+
+        let myDivOverlay = document.createElement("div");
+        myDivOverlay.className = "img-overlay img-overlay-blur";
+
+        let myDivImgTitle = document.createElement("div");
+        myDivImgTitle.className = "img-title";
+        myDivImgTitle.textContent = listadoPeliculas[i].title;
+
+        let myDivImgSubtitle = document.createElement("div");
+        myDivImgSubtitle.className = "img-subtitle";
+        myDivImgSubtitle.textContent = "Reservar";
+
+        row.appendChild(myDivRow1);
+        myDivRow1.appendChild(myLink);
+        myLink.appendChild(myImg);
+        myLink.appendChild(myDivOverlay);
+        myDivOverlay.appendChild(myDivImgTitle);
+        myDivOverlay.appendChild(myDivImgSubtitle);
+    }
+}
